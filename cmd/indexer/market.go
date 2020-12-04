5d999ced52dd10700eeeaf0469a4edae4e5e6c34
Hector Sanjuan <code@hector.link>
2020-12-04 15:59:28 +0100

    yes

--- cmd/indexer/market.go
+++ cmd/indexer/market.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 
+	lotusmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
+	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/sentinel-visor/lens"
 	"github.com/filecoin-project/sentinel-visor/model/actors/market"
@@ -10,11 +12,54 @@ import (
 )
 
 func extractMarket(ctx context.Context, node lens.API, chain *chain, actor *types.Actor, info actorstate.ActorInfo, results *market.MarketTaskResult) error {
-	eae := actorstate.StorageMarketExtractor{}
-	res, err := eae.Extract(ctx, info, node)
+	m := actorstate.StorageMarketExtractor{}
+
+	pred := state.NewStatePredicates(node)
+	marketPred := actorstate.StorageMarketChangesPred(pred)
+
+	curState, err := lotusmarket.Load(node.Store(), actor)
+	if err != nil {
+		return err
+	}
+
+	prevActor, err := chain.prevState.GetActor(info.Address)
+	if err != nil {
+		return err
+	}
+
+	prevState, err := lotusmarket.Load(node.Store(), prevActor)
 	if err != nil {
 		logger.Error(err)
+		return err
 	}
-	*results = *res.(*market.MarketTaskResult)
+
+	changed, val, err := marketPred(ctx, prevState, curState)
+	if err != nil {
+		return err
+	}
+	if !changed {
+		return err
+	}
+
+	mchanges, ok := val.(*actorstate.MarketChanges)
+	if !ok {
+		return err
+	}
+
+	if mchanges.ProposalChanges != nil {
+		proposals, err := m.MarketDealProposalChanges(ctx, info, mchanges.ProposalChanges)
+		if err != nil {
+			return err
+		}
+		results.Proposals = append(results.Proposals, proposals...)
+	}
+
+	if mchanges.DealChanges != nil {
+		states, err := m.MarketDealStateChanges(ctx, info, mchanges.DealChanges)
+		if err != nil {
+			return err
+		}
+		results.States = append(results.States, states...)
+	}
 	return nil
 }
